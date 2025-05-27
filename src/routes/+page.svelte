aaa3f8773d79f425fa26ded4d16200590c3a5ab9
Joseph Jackson <joejak93@gmail.com>
2025-05-27 15:02:48 -0500

    init

---
+++ src/routes/+page.svelte
@@ -0,0 +1,26 @@
+<a
+  href="https://rapesco.com/kd733/wp-content/uploads/2022/04/94-pencil-sharpener-instructions.pdf"
+  target="_blank"
+  rel="noopener noreferrer"
+>
+  <button>NEW TAB</button>
+</a>
+<a
+  href="https://rapesco.com/kd733/wp-content/uploads/2022/04/94-pencil-sharpener-instructions.pdf"
+>
+  <button> open over </button></a
+>
+
+<style>
+  * {
+    color: #1d0707;
+  }
+  a {
+    display:block;
+  }
+  button{
+    margin: 10px;
+    height: 50px; 
+    width: 80vw;
+  }
+</style>
